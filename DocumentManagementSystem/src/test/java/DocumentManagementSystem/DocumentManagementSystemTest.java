f5f8eeaa7ca8c6eab087c44c53fbff29b12565a4
luffy1610 <luffy1610@gmail.com>
2021-02-26 22:57:43 +0800

    test: Add tests for text file types
    Add assertAttributeEquals helper function
    Refactor importImageAttributes with helper function
    Add tests for text file types: LETTER, REPORT, INVOICE

--- DocumentManagementSystem/src/test/java/DocumentManagementSystem/DocumentManagementSystemTest.java
+++ DocumentManagementSystem/src/test/java/DocumentManagementSystem/DocumentManagementSystemTest.java
@@ -12,14 +12,65 @@ public class DocumentManagementSystemTest {
     private static final String RESOURCES =
             "src" + File.separator + "test" + File.separator + "resources" + File.separator;
     private static final String IMAGE = RESOURCES + "test.jpg";
-	
-	@Test
-	public void importImagaAttributes() throws IOException {
-		final MainApplication mainApplication = new MainApplication();
-		mainApplication.importFile(IMAGE);
-		final Document doc = mainApplication.contents().get(0);
-		assertEquals(doc.getAttribute(WIDTH), "800");
-		assertEquals(doc.getAttribute(HEIGHT), "455");
-	}
+    private static final String LETTER = RESOURCES + "test.letter";
+    private static final String REPORT = RESOURCES + "test.report";
+    private static final String INVOICE = RESOURCES + "test.invoice";
 
+    private MainApplication mainApplication = new MainApplication();
+    
+    @Test
+    public void importImagaAttributes() throws IOException {
+        mainApplication.importFile(IMAGE);
+        final Document document = mainApplication.contents().get(0);
+        assertAttributeEquals(document, TYPE, "IMAGE");
+        assertAttributeEquals(document, WIDTH, "800");
+        assertAttributeEquals(document, HEIGHT, "455");
+    }
+
+    @Test
+    public void importLetterAttributes() throws IOException {
+        mainApplication.importFile(LETTER);
+        final Document document = mainApplication.contents().get(0);
+        assertAttributeEquals(document, TYPE, "LETTER");
+        assertAttributeEquals(document, PATIENT, "Joe Bloggs");
+        assertAttributeEquals(document, ADRRESS, 
+            "123 Fake Street\n" +
+            "Westminster\n" +
+            "London\n" +
+            "United Kingdom");
+        assertAttributeEquals(document, BODY,
+            "We are writing to you to confirm the re-scheduling of your appointment\n" +
+            "with Dr. Avaj from 29th December 2016 to 5th January 2017.");
+    }
+
+    @Test
+    public void importReportAttributes() throws IOException {
+        mainApplication.importFile(REPORT);
+        final Document document = mainApplication.contents().get(0);
+        assertAttributeEquals(document, TYPE, "REPORT");
+        assertAttributeEquals(document, PATIENT, "Joe Bloggs");
+        assertAttributeEquals(document, BODY,
+            "On 5th January 2017 I examined Joe's teeth.\n" +
+            "We discussed his switch from drinking Coke to Diet Coke.\n" +
+            "No new problems were noted with his teeth.");
+    }
+
+    @Test
+    public void importInvoiceAttributes() throws IOException {
+        mainApplication.importFile(INVOICE);
+        final Document document = mainApplication.contents().get(0);
+        assertAttributeEquals(document, TYPE, "INVOICE");
+        assertAttributeEquals(document, PATIENT, JOE_BLOGGS);
+        assertAttributeEquals(document, AMOUNT, "$100");
+    }
+
+    private void assertAttributeEquals(
+        final Document document,
+        final String attributeName,
+        final String expectedValue) {
+
+        assertEquals(
+            "Document has the wrong value for " + attributeName,
+            expectedValue, document.getAttribute(attributeName));
+    }
 }
